d3141caa4b5ba863e5b6172956f50726dbd4cafc
danyulgh <daniel.g.hoang2007@gmail.com>
2025-08-24 22:40:43 -0400

    component injection

--- packages/shared/lib/utils/index.ts
+++ packages/shared/lib/utils/index.ts
@@ -1,4 +1,5 @@
 export * from './helpers.js';
 export * from './colorful-logger.js';
 export * from './init-app-with-shadow.js';
+export * from './init-component-with-shadow.js';
 export type * from './types.js';
---
+++ packages/shared/lib/utils/init-component-with-shadow.ts
@@ -0,0 +1,49 @@
+import { createRoot } from 'react-dom/client';
+import type { ReactElement } from 'react';
+
+export const initComponentWithShadow = ({
+  id,
+  app,
+  inlineCss,
+  target,
+  placement,
+}: {
+  id: string;
+  inlineCss: string;
+  app: ReactElement;
+  target: string;
+  placement: 'afterend' | 'beforebegin' | 'child';
+}) => {
+  const root = document.createElement('div');
+  root.id = id;
+
+  const targetElement = document.querySelector(target);
+  if (targetElement === null) return;
+  if (placement === 'child') targetElement.appendChild(root);
+  else targetElement.insertAdjacentElement(placement, root);
+
+  const rootIntoShadow = document.createElement('div');
+  rootIntoShadow.id = `shadow-root-${id}`;
+
+  const shadowRoot = root.attachShadow({ mode: 'open' });
+
+  if (navigator.userAgent.includes('Firefox')) {
+    /**
+     * In the firefox environment, adoptedStyleSheets cannot be used due to the bug
+     * @url https://bugzilla.mozilla.org/show_bug.cgi?id=1770592
+     *
+     * Injecting styles into the document, this may cause style conflicts with the host page
+     */
+    const styleElement = document.createElement('style');
+    styleElement.innerHTML = inlineCss;
+    shadowRoot.appendChild(styleElement);
+  } else {
+    /** Inject styles into shadow dom */
+    const globalStyleSheet = new CSSStyleSheet();
+    globalStyleSheet.replaceSync(inlineCss);
+    shadowRoot.adoptedStyleSheets = [globalStyleSheet];
+  }
+
+  shadowRoot.appendChild(rootIntoShadow);
+  createRoot(rootIntoShadow).render(app);
+};
