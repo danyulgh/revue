6cf03b3322a94b6f91ff8dde1ead3c0e66ecdd4b
danyulgh <daniel.g.hoang2007@gmail.com>
2025-08-30 23:28:17 -0400

    add district list

--- packages/shared/lib/utils/index.ts
+++ packages/shared/lib/utils/index.ts
@@ -2,4 +2,5 @@ export * from './helpers.js';
 export * from './colorful-logger.js';
 export * from './init-app-with-shadow.js';
 export * from './init-component-with-shadow.js';
+export * from './districts.js';
 export type * from './types.js';
