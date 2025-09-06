96f78d573b7151d6385969e59d59530e2643c982
danyulgh <daniel.g.hoang2007@gmail.com>
2025-09-06 14:01:46 -0400

    update css border

--- packages/ui/lib/components/ui/radio-group.tsx
+++ packages/ui/lib/components/ui/radio-group.tsx
@@ -18,7 +18,7 @@ const RadioGroupItem = React.forwardRef<
   <RadioGroupPrimitive.Item
     ref={ref}
     className={cn(
-      'border-primary text-primary focus-visible:ring-ring aspect-square h-4 w-4 rounded-full border shadow focus:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50',
+      'border-border text-primary focus-visible:ring-ring aspect-square h-4 w-4 rounded-full border shadow focus:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50',
       className,
     )}
     {...props}>
